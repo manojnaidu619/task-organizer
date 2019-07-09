cbd9f531ea9a9d4838843c077c7013a03ff26eb0
Manoj Naidu <manojnaidu619@gmail.com>
2019-07-09 22:31:41 +0530

    Added hard-coded list data

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Earth!'
+    tasks: [
+      {id:1, name: 'Task 1', description: 'Just some ranodm one', completed: true},
+      {id:2, name: 'Task 2', description: 'Just as it is!', completed: false},
+      {id:3, name: 'Task 3', description: 'some ranodm one', completed: true},
+      {id:4, name: 'Task 4', description: 'Chill,for fun!', completed: false},
+      {id:5, name: 'Task 5', description: 'Just hanging out here!', completed: true}
+    ]
   }
 })
