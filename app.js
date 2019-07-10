a1c337ab1c2fc6820fab6061ff720f8a10d98761
Manoj Naidu <manojnaidu619@gmail.com>
2019-07-10 11:31:05 +0530

    Remove redundant code

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ var app = new Vue({
   components: {
     'task' : {
               props: ['task'],
-              template: `
-                  <div class="ui segment task" v-bind:class="task.completed ? 'done' : 'todo' " v-for='task in todoTasks'>
-                    Name : {{task.name}} description: {{task.description}} Status: {{task.completed}}
-                  </div>
-              `
+              template: `<div class="ui segment task" v-bind:class="task.completed ? 'done' : 'todo'" >
+                           Name : {{task.name}} description: {{task.description}} Status: {{task.completed}}
+                         </div>`
              }
     },
   data: {
